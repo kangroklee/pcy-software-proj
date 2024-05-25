13730b298d14e647cd6734e385a70af91bb691c3
dylan <nyskyline7@gmail.com>
2024-05-26 00:55:08 +0900

    add SearchPictureFrame

--- Test.java
+++ Test.java
@@ -1,15 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.TitledBorder;
 
 class AddPictureFrame extends JFrame {
     public AddPictureFrame() {
         super("Add a Picture");
         JPanel topPanel = new JPanel();
         topPanel.add(new JLabel("Time"));
-        topPanel.add(new JTextField(20));
+        topPanel.add(new JTextField(15));
         topPanel.add(new JLabel("(Picture) Tags"));
-        topPanel.add(new JTextField(20));
+        topPanel.add(new JTextField(15));
         add(topPanel, BorderLayout.NORTH);
 
         add(new JButton("Select Image File"), BorderLayout.CENTER);
@@ -21,10 +22,74 @@ class AddPictureFrame extends JFrame {
         add(commentsPanel, BorderLayout.SOUTH);
 
         JPanel bottomPanel = new JPanel();
-        bottomPanel.add(new JButton("More Stuff"), BorderLayout.WEST);
-        bottomPanel.add(new JButton("OK - Input End"), BorderLayout.EAST);
+        bottomPanel.add(new JButton("More Stuff"), BorderLayout.LINE_START);
+        bottomPanel.add(new JButton("OK - Input End"), BorderLayout.LINE_END);
         add(bottomPanel, BorderLayout.SOUTH);
 
+        this.pack();
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+}
+
+class SearchPictureFrame extends JFrame {
+    public SearchPictureFrame() {
+        super("Search Picture");
+        // Create the Time Search panel with a titled border
+        JPanel timeSearchPanel = new JPanel();
+        timeSearchPanel.setBorder(new TitledBorder("Time Search"));
+        timeSearchPanel.setLayout(new GridLayout(2, 3, 5, 5));
+        timeSearchPanel.add(new JLabel("From"));
+        timeSearchPanel.add(new JTextField());
+        timeSearchPanel.add(new JLabel("(yyyy-MM-dd_HH:mm:ss)"));
+        timeSearchPanel.add(new JLabel("To"));
+        timeSearchPanel.add(new JTextField());
+
+        // Create the Keyword Search panel with a titled border
+        JPanel keywordSearchPanel = new JPanel();
+        keywordSearchPanel.setBorder(new TitledBorder("Keyword Search"));
+        keywordSearchPanel.setLayout(new BoxLayout(keywordSearchPanel, BoxLayout.PAGE_AXIS));
+        
+        JPanel detailSearchPanel = new JPanel();
+        
+
+        JPanel metaSearchPanel = new JPanel();
+        metaSearchPanel.setLayout(new GridLayout(2, 2));
+        metaSearchPanel.add(new JLabel("Tags"));
+        metaSearchPanel.add(new JTextField(10));
+        metaSearchPanel.add(new JLabel("Comments"));
+        metaSearchPanel.add(new JTextField(10));
+        detailSearchPanel.add(metaSearchPanel, BorderLayout.WEST);
+
+        JPanel stuffSearchPanel = new JPanel();
+        stuffSearchPanel.setLayout(new GridLayout(3, 2, 1, 5));
+        stuffSearchPanel.add(new JLabel("Type"));
+        stuffSearchPanel.add(new JTextField(10));
+        stuffSearchPanel.add(new JLabel("Name"));
+        stuffSearchPanel.add(new JTextField(10));
+        stuffSearchPanel.add(new JLabel("Tags"));
+        stuffSearchPanel.add(new JTextField(10));
+        detailSearchPanel.add(stuffSearchPanel, BorderLayout.EAST);
+
+        JPanel generalSearchPanel = new JPanel();
+        generalSearchPanel.add(new JLabel("General Search"));
+        generalSearchPanel.add(new JTextField(10));
+        
+        keywordSearchPanel.add(detailSearchPanel, BorderLayout.CENTER);
+        keywordSearchPanel.add(generalSearchPanel, BorderLayout.SOUTH);
+
+        // Create the buttons panel
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.add(new JButton("AND SEARCH"));
+        buttonsPanel.add(new JButton("OR SEARCH"));
+        buttonsPanel.add(new JButton("CLOSE"));
+
+        // Add the panels to the main frame
+        add(timeSearchPanel, BorderLayout.NORTH);
+        add(keywordSearchPanel, BorderLayout.CENTER);
+        add(buttonsPanel, BorderLayout.SOUTH);
+
+        // Display the frame
         this.pack();
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
@@ -44,7 +109,7 @@ public class Test {
         frame.add(new JButton("Show All Pictures"), BorderLayout.NORTH);
         
         //1. make a PictureList
-        PictureList a = new PictureList("/Users/dylan/picture-normal-gui.data");
+        PictureList a = new PictureList("static/picture-normal-gui.data");
         //2. feed PictureList to PictureSection
         JScrollPane ps = new PictureSection(a).getSection();
 
@@ -66,7 +131,16 @@ public class Test {
         sideBar.add(new JButton("DELETE"));
         sideBar.add(new JButton("LOAD"));
         sideBar.add(new JButton("SAVE"));
-        sideBar.add(new JButton("SEARCH"));
+        JButton searchPictureButton = new JButton("SEARCH");
+        searchPictureButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Create and display the new JFrame
+                SearchPictureFrame f = new SearchPictureFrame();
+                f.setVisible(true);
+            }
+        });
+        sideBar.add(searchPictureButton);
         
         frame.add(sideBar, BorderLayout.EAST);
         
